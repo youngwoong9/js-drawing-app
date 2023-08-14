36e59ea346f2d847c43ed4dfdc8db937e4a9dad1
youngwoong9 <youngwoong9@naver.com>
2023-08-14 13:38:39 +0900

    Drawing a human

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(250, 200, 150, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(200, 200, 20, 100);
+ctx.fillRect(350, 200, 20, 100);
+
+ctx.fillRect(260, 200, 50, 200);
+ctx.arc(285, 140, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(265, 130, 10, Math.PI, 2 * Math.PI);
+ctx.arc(305, 130, 10, Math.PI, 2 * Math.PI);
 ctx.fill();
